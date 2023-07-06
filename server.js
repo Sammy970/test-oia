4335046613bd4afbaa607114ef6e76f9f9e5ee4e
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-06 17:59:54 +0530

    test

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ app.get("/abcd", (req, res) => {
   const ipAddress =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   console.log(ipAddress);
-  res.redirect("http://test-oia.vercel.app/abcd?link=https://www.instagram.com/my_art_craft");
+
+  // Check if the request is coming from a mobile device
+  const userAgent = req.headers["user-agent"];
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      userAgent
+    );
+
+  if (isMobile) {
+    // Redirect to the deep link URL for the app
+    res.redirect("instagram://user?username=my_art_craft");
+  } else {
+    // Redirect to a web URL as a fallback for non-mobile devices
+    res.redirect("https://www.instagram.com/my_art_craft");
+  }
 });
 
 app.listen(port, () => {

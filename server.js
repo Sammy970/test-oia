9655cc23a768d8cbb9909d57f7fc48074493067a
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-07 20:45:17 +0530

    Changes Added

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // Importing Dependencies
 const express = require("express");
 const { nanoid } = require("nanoid");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const ogs = require("open-graph-scraper");
 
-// Importing Function
-const { fetchOGMetadata } = require("./functions");
+const metascraper = require("metascraper")([
+  require("metascraper-description")(),
+  require("metascraper-title")(),
+  require("metascraper-url")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-author")(),
+]);
+const got = require("got");
 
 // Important Settings
 const app = express();
@@ -15,9 +25,9 @@ app.set("trust proxy", true);
 // Object to store generated codes and their corresponding links
 const codes = {};
 
-// app.get("/", (req, res) => {
-//   res.send("good");
-// });
+app.get("/", (req, res) => {
+  res.send("good");
+});
 
 app.get("/generate", async (req, res) => {
   const link = req.query.link;
@@ -60,17 +70,71 @@ app.get("/:code", (req, res) => {
   }
 });
 
+// async function fetchOGMetadata(url) {
+//   try {
+//     const response = await axios.get(url);
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     const ogMetadata = {};
+
+//     $("head meta[property^='og:']").each((index, element) => {
+//       const property = $(element).attr("property");
+//       const content = $(element).attr("content");
+//       ogMetadata[property] = content;
+//     });
+
+//     console.log(ogMetadata);
+
+//     return ogMetadata;
+//   } catch (error) {
+//     console.error("Error fetching Open Graph metadata:", error);
+//     return null;
+//   }
+// }
+
+// async function fetchOGMetadata(url) {
+//   try {
+//     const options = { url };
+//     const { result } = await ogs(options);
+//     // const ogMetadata = result ? result.ogTitle : null;
+
+//     console.log(result);
+
+//     return result;
+//   } catch (error) {
+//     console.error("Error fetching Open Graph metadata:", error);
+//     return null;
+//   }
+// }
+
+async function fetchOGMetadata(url) {
+  try {
+    const { body: html, url: finalUrl } = await got(url);
+    const metadata = await metascraper({ html, url: finalUrl });
+
+    console.log(metadata);
+
+    return metadata;
+  } catch (error) {
+    console.error("Error fetching Open Graph metadata:", error);
+    return null;
+  }
+}
+
 function generateHTMLWithOGMetadata(link, ogMetadata) {
   if (!ogMetadata) {
     return `<html><head><meta http-equiv="refresh" content="0; url=${link}" /></head></html>`;
   }
 
-  const metaTags = Object.entries(ogMetadata)
-    .map(
-      ([property, content]) =>
-        `<meta property="${property}" content='${content}' />`
-    )
-    .join("\n");
+  let metaTags = "";
+
+  for (const property in ogMetadata) {
+    if (Object.hasOwnProperty.call(ogMetadata, property)) {
+      const content = ogMetadata[property];
+      // console.log(content);
+      metaTags += `<meta property="og${property}" content="${content}" />`;
+    }
+  }
 
   const html = `
     <html>
